1d849d30473ae63634c32a712536145b05fbe1fc
zhouxun <zhouxun01@beantechs.cn>
2020-05-13 14:54:53 +0800

    初始化

--- src/test/java/com/lock/DistributionLockApplicationTests.java
+++ src/test/java/com/lock/DistributionLockApplicationTests.java
@@ -1,5 +1,6 @@
 package com.lock;
 
+import com.lock.etcd.EtcdLock;
 import com.lock.redis.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,16 +13,40 @@ class DistributionLockApplicationTests {
     private RedisUtil redisUtil;
 
     @Test
-    public void lock() {
+    public void redisLock() {
         boolean res1 = redisUtil.tryGetDistributedLock("test", "10010", 100);
         System.out.println(res1);
         boolean res2 = redisUtil.tryGetDistributedLock("test", "10010", 100);
         System.out.println(res2);
+        boolean res = redisUtil.releaseDistributedLock("test", "10010");
+        System.out.println(res);
     }
 
     @Test
-    public void releaseLock() {
-        boolean res = redisUtil.releaseDistributedLock("test", "10010");
-        System.out.println(res);
+    public void etcdLock() {
+        for (int i = 0; i < 10; i++) {
+            new MyThread().start();
+        }
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            System.out.println("[error]:" + e);
+        }
+    }
+
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            String lockName = "/lock";
+            EtcdLock.LockResult lockResult = EtcdLock.getInstance().lock(lockName, 30);
+            if (lockResult.getIsLockSuccess()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    System.out.println("[error]:" + e);
+                }
+            }
+            EtcdLock.getInstance().unLock(lockName, lockResult);
+        }
     }
 }
